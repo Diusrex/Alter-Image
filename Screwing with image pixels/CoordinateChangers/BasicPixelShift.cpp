d0b4fddcf96ed306c2e3eab73021c9509069170e
Diusrex <killakan002@hotmail.com>
2015-02-24 17:55:34 -0700

    Added a license

--- Screwing with image pixels/CoordinateChangers/BasicPixelShift.cpp
+++ Screwing with image pixels/CoordinateChangers/BasicPixelShift.cpp
@@ -1,3 +1,18 @@
+/*
+   Copyright 2015 Morgan Redshaw
+  
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+  
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 #include "BasicPixelShift.h"
 
 #include "../PixelCoordinate.h"
@@ -34,6 +49,6 @@ PixelCoordinate BasicPixelShift::GetShiftedPosition(const Coordinate &origionalC
 bool BasicPixelShift::ShouldShift(const Coordinate & origionalCoordinate)
 {
     return origionalCoordinate.x % xShiftEveryPixels == 0 && origionalCoordinate.y % yShiftEveryPixels == 0;
-}
+}   
 
 } // CoordinateChanger
